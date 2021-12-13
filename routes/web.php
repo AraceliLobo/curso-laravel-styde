0301669d2ab6cfedb1e97d3fba56919ac736b628
Araceli <araceli.lobo@sotysolar.es>
2021-12-13 16:20:48 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,26 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $notes = [
+        'Mi primera nota',
+        'Mi segunda nota',
+        'Mi tercera nota',
+        'Mi cuarta nota',
+    ];
+
+
+    return view('notes', ['notes' => $notes]);  
+})->name('notes');
+
+Route::get('notas/{id}', function($id){
+    return 'Aquí veremos el detalle de la nota '.$id;
+})->name('notes.show');
+// ->where('id', '\d+')
+
+Route::get('notas/crear', function() {
+    return view('add-note');
+})->name('notes.create');
+
+Route::get('notas/{id}/editar', function($id){
+    return 'Aquí podemos editar la nota: '.$id;
+})->name('notes.edit');
